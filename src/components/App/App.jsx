1b7f7a34691167f66efdc4d5fa2ab494c31d1258
miamarichka <ilchyshyn.marichka@gmail.com>
2023-03-29 00:51:13 +0300

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { getData } from "api/fetchData";
 import { Searchbar } from "../Searchbar/Searchbar";
